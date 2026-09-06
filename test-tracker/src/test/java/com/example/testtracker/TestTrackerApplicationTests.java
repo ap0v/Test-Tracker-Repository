952bad817c094dfa66eb2ab18c31dc1781721cea
AlandisAyupov <steamuservni@gmail.com>
2026-09-06 17:42:43 -0400

    chore: initialize Spring Boot project

---
+++ test-tracker/src/test/java/com/example/testtracker/TestTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testtracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
