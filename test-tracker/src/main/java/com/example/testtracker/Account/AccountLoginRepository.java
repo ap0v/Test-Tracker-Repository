a69e692de9b8554f0eb9cba86f69f5536a253df2
AlandisAyupov <steamuservni@gmail.com>
2026-09-19 18:50:25 -0400

    feat: implement AccountLoginRepository

--- test-tracker/src/main/java/com/example/testtracker/Account/AccountLoginRepository.java
+++ test-tracker/src/main/java/com/example/testtracker/Account/AccountLoginRepository.java
@@ -1,5 +1,18 @@
 package com.example.testtracker.Account;
 
-public class AccountLoginRepository {
-    
+import java.util.Optional;
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+/**
+ * Repository interface for managing AccountLogin entities.
+ */
+public interface AccountLoginRepository extends JpaRepository<AccountLogin, UUID> {
+    @Query(value = """
+        select * from account_logins where lower(btrim(email)) = lower(btrim(:email))
+    """, nativeQuery = true)
+    Optional<AccountLogin> findByNormalizedEmail(@Param("email") String email);
 }
