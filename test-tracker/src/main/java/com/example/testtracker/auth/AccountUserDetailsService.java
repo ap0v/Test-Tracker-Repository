2b56ca5e8f4c46789d97bb32949186b700d8e67e
AlandisAyupov <steamuservni@gmail.com>
2026-09-19 19:36:42 -0400

    feat: implement AccountUserDetailsService.java

--- test-tracker/src/main/java/com/example/testtracker/auth/AccountUserDetailsService.java
+++ test-tracker/src/main/java/com/example/testtracker/auth/AccountUserDetailsService.java
@@ -1,5 +1,25 @@
 package com.example.testtracker.auth;
 
-public class AccountUserDetailsService {
+import com.example.testtracker.AccountLogin.AccountLogin;
+import com.example.testtracker.AccountLogin.AccountLoginRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+@Service
+public class AccountUserDetailsService implements UserDetailsService {
+    private final AccountLoginRepository accountLoginRepository;
+
+    public AccountUserDetailsService(AccountLoginRepository accountLoginRepository) {
+        this.accountLoginRepository = accountLoginRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        AccountLogin accountLogin = accountLoginRepository.findByNormalizedEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with email: " + email));
+                
+        return new AccountLoginPrincipal(accountLogin);
+    }
     
 }
